adb5c406e0eb4a237ae131517855794389e3d722
Lasya M S <lasyams@gmail.com>
2018-04-07 16:28:43 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+(function(){
+'use strict';
+angular.module('LunchCheck',[])
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController($scope){
+    $scope.message = "";
+
+    $scope.checklunch = function(){
+      var items;
+      if($scope.input){
+            //$scope.input = $scope.input.trim();
+          items = $scope.input.split(",");
+          items = items.filter(function(items){
+            return items.length >0;
+            });
+            if (items.length <= 3) {
+                      $scope.message = "Enjoy!";
+                    } else {
+                      $scope.message = "Too much!";
+                    }
+            }
+         else {
+          $scope.message = "Please enter data first";
+        }
+      }
+    }
+
+})();
